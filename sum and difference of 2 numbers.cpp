12c2cbd44baeba5312574732ddde504a5e63855c
satyasuguna2007-dot <satyasuguna2007@gmail.com>
2025-11-08 16:00:16 +0530

    Add files via upload

---
+++ sum and difference of 2 numbers.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,b,sum,diff;
+	printf("enter a and b values");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	diff=a-b;
+	printf("sum of digits=%d",sum);
+	printf("diff of digits=%d",diff);
+	}
