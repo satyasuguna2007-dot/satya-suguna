a18edcd9372893138d82fa35b343d0ee13a6a44b
satyasuguna2007-dot <satyasuguna2007@gmail.com>
2025-11-08 15:56:52 +0530

    Add files via upload

---
+++ difference of 2 numbers.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+	printf("enter a and b  values");
+	scanf("%d %d", &a,&b);
+	sum=a+b;
+	diff=a-b;
+	printf("sum of digits =%d",sum);
+	printf("diff of digits = %d",diff);
+}
